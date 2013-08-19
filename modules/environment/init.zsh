e6fa76939aa5c4477c0264139b02adec13d2cfb1
Georges Discry <georges@discry.be>
2013-08-19 11:14:24 +0200

    Set TERM to *-256color for adequate terminals

--- modules/environment/init.zsh
+++ modules/environment/init.zsh
@@ -34,6 +34,32 @@ unsetopt HUP              # Don't kill jobs on shell exit.
 unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 
 #
+# 256 Colors
+#
+
+local256="$COLORTERM$XTERM_VERSION$ROXTERM_ID$KONSOLE_DBUS_SESSION"
+
+if [[ -n "$local256" ]]; then
+
+  case "$TERM" in
+    'xterm')
+      TERM=xterm-256color;;
+    'screen')
+      TERM=screen-256color;;
+    'Eterm')
+      TERM=Eterm-256color;;
+  esac
+  export TERM
+
+  if [[ -n "$TERMCAP" && "$TERM" = "screen-256color" ]]; then
+    TERMCAP=$(echo "$TERMCAP" | sed -e 's/Co#8/Co#256/g')
+    export TERMCAP
+  fi
+fi
+
+unset local256
+
+#
 # Grep
 #
 
